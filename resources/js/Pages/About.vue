0e5f894127c1baca5e09714b43f0bcae6258b13c
kelviinprawira <kelviinprawira@gmail.com>
2024-07-30 10:30:07 +0700

    template vilt stack

---
+++ resources/js/Pages/About.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <h1>About Page {{ users }}</h1>
+    </div>
+</template>
+
+<script setup>
+defineProps({
+    users: String,
+});
+</script>
---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,7 @@
+<script></script>
+
+<template>
+    <div>
+        <h1 class="text-2xl">anjai mabar</h1>
+    </div>
+</template>
